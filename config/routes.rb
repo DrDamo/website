82f1b96f18813af00b8a0a486ed3f8b5186c1f8b
Jeremy Walker <jeremy@meducation.net>
2013-01-19 18:13:34 +0000

    Add fake attendees section

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Website::Application.routes.draw do
 
   get 'what' => 'pages#what', as: :what
   get 'teachers' => 'pages#teachers', as: :teachers
+  get 'attendees' => 'pages#attendees', as: :attendees
+  
+  post 'login' => 'pages#attendees', as: :login
   
   root :to => 'pages#home'
 end
